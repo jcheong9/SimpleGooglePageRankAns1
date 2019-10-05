d7792f45778d4f65125a9729723936cc27fcb172
Jameson Cheong <jctheathlete@gmail.com>
2019-10-05 01:10:09 -0700

    add comments

--- ConnectivityMatrix.cpp
+++ ConnectivityMatrix.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "ConnectivityMatrix.hpp"
+//create connectivity matrix
 void ConnectivityMatrix::makeConnectivityMatrix() {
     this->set_value(0,1,1);
     this->set_value(0,2,1);
@@ -11,5 +12,5 @@ void ConnectivityMatrix::makeConnectivityMatrix() {
     this->set_value(2,0,1);
     this->set_value(2,1,1);
 }
-
+//empty constructor taking in vector
 ConnectivityMatrix::ConnectivityMatrix(vector<double> vector) : Matrix(vector) {}
--- main.cpp
+++ main.cpp
@@ -17,18 +17,15 @@ int main() {
     //read file
     readFile(&numInText);
     ConnectivityMatrix S{numInText};
-
     //set up S and Q for summation
     setClickLinksAndTeleport(&S, &Q);
-
     //add S and Q to get M
     M = (S + Q);
-
+    //processing matrix using Markov
     setRankMatrixToOnes(&rankMat);
     rankMat = applyMarkovProcess(M, rankMat);
     //print out result and format
     printRanking( rankMat);
-
     return 0;
 }
 void printRanking(Matrix rankMat){
@@ -37,7 +34,6 @@ void printRanking(Matrix rankMat){
     cout << "Page C: " << to_string(rankMat.get_value(2,0)).substr(0, 5) << "%" << endl;
     cout << "Page D: " << to_string(rankMat.get_value(3,0)).substr(0, 4) << "%" << endl;
 }
-
 
 void readFile(vector<double> *numInText){
     ifstream infile;
